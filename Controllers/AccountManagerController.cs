1a0fc0f70e36c48963977c269c1703338eaafad7
Subtype3860 <113200110+Subtype3860@users.noreply.github.com>
2023-11-05 22:30:58 +0300

    SaveProject (проект не закончен)

---
+++ Controllers/AccountManagerController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskTeamWork.Models;
+using TaskTeamWork.ViewModels.Account;
+
+namespace TaskTeamWork.Controllers;
+
+public class AccountManagerController : Controller
+{
+    private IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    public AccountManagerController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _mapper = mapper;
+    }
+    
+    [Route("Login")]
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View("Login");
+    }
+    [Route("Login")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+               
+            var user = _mapper.Map<User>(model);
+
+            var result = await _signInManager.PasswordSignInAsync(user.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            }
+        }
+        return View("Views/Home/Index.cshtml");
+    }
+
+    [Route("Logout")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
