97f4b53c35fc86948797ab0a244d7f2da8d394e2
Subtype3860 <113200110+Subtype3860@users.noreply.github.com>
2023-11-07 20:44:54 +0300

    CorectProject

--- Models/User.cs
+++ Models/User.cs
@@ -4,5 +4,8 @@ namespace TaskTeamWork.Models;
 
 public class User : IdentityUser
 {
-    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public DateTime BirthDate { get; set; }    
 }
